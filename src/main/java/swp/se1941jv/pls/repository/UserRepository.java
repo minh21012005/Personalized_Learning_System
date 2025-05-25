805085fb123cb45e859bc6c7c60e3a556afd0b84
Nguyễn Bá Minh <minhhmoii@gmail.com>
2025-05-25 15:17:48 +0700

    validate duplicate phone number

--- src/main/java/swp/se1941jv/pls/repository/UserRepository.java
+++ src/main/java/swp/se1941jv/pls/repository/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
 
     boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(String phone);
 }
